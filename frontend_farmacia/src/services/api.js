9847391d4b2aa1a6f0910fffdc83af5bd2ac5e79
Roberto Palape <roberto.flores.palape@gmail.com>
2025-08-28 07:15:52 -0400

    Usar backend de Render en producción

--- frontend_farmacia/src/services/api.js
+++ frontend_farmacia/src/services/api.js
@@ -7,7 +7,7 @@ import axios from "axios";
 
 // Crear instancia de Axios
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8080/api/v1",
+  baseURL: import.meta.env.VITE_API_BASE_URL ,
   headers: {
     "Content-Type": "application/json",
   },
